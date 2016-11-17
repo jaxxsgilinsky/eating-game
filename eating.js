ad6cc88d115f0a90c4031099ee813c917902b2ce
Jackie Guzman <jackieguzman3241@gmail.com>
2016-11-17 08:58:17 -0800

    finished part 3.

--- eating.js
+++ eating.js
@@ -1,28 +1,28 @@
-var duck = document.getElementById("duck");
-var duckXVal = Number(duck.getAttribute("x"));
-var duckYVal = Number(duck.getAttribute("y"));
+var dog = document.getElementById("dog");
+var dogXVal = Number(dog.getAttribute("x"));
+var dogYVal = Number(dog.getAttribute("y"));
 
 
 document.addEventListener("keydown", function(e) {
 
   if (e.keyCode == 37) {
-    duck.setAttribute("x",duckXVal - 15);
-  duckXVal = duckXVal - 15;
+    dog.setAttribute("x",dogXVal - 15);
+  dogXVal = dogXVal - 15;
   }
 
    else if (e.keyCode == 39) {
-    duck.setAttribute("x",duckXVal + 15);
-  duckXVal = duckXVal + 15;
+    dog.setAttribute("x",dogXVal + 15);
+  dogXVal = dogXVal + 15;
   }
 
 
    else if (e.keyCode == 38) {
-    duck.setAttribute("y",duckYVal - 15);
-  duckYVal = duckYVal - 15;
+    dog.setAttribute("y",dogYVal - 15);
+  dogYVal = dogYVal - 15;
   }
 
    else if (e.keyCode == 40) {
-    duck.setAttribute("y",duckYVal + 15);
-  duckYVal = duckYVal + 15;
+    dog.setAttribute("y",dogYVal + 15);
+  dogYVal = dogYVal + 15;
   }
 })
