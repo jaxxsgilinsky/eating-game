f9144fcc9bc9f89458a73e0bcdc6448fa78ac40b
Jackie Guzman <jackieguzman3241@gmail.com>
2016-11-29 09:22:39 -0800

    finished part 4

--- eating.js
+++ eating.js
@@ -1,6 +1,8 @@
 var dog = document.getElementById("dog");
 var dogXVal = Number(dog.getAttribute("x"));
 var dogYVal = Number(dog.getAttribute("y"));
+var height = 40;
+var width= 40;
 
 
 document.addEventListener("keydown", function(e) {
@@ -24,5 +26,21 @@ document.addEventListener("keydown", function(e) {
    else if (e.keyCode == 40) {
     dog.setAttribute("y",dogYVal + 15);
   dogYVal = dogYVal + 15;
+}
+
+  var ballX = Number(document.getElementById("food1").getAttribute("x"))
+  var ballY = Number(document.getElementById("food1").getAttribute("y"))
+
+  if (dogXVal > ballX && dogXVal < ballX + width && dogYVal > ballY && dogYVal < ballY + height) {
+    console.log("overlap");
   }
+
+  ballX = Number(document.getElementById("food2").getAttribute("x"))
+  ballY = Number(document.getElementById("food2").getAttribute("y"))
+
+  if (dogXVal > ballX && dogXVal < ballX + width && dogYVal > ballY && dogYVal < ballY + height) {
+    console.log("overlap");
+  }
+
+
 })
